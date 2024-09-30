39ea839de1d3d6bf06e39c831c9a0cfb06ee074d
Fabio D'Agostino <fabio.dagostino@descor.com>
2024-09-30 17:35:33 +0200

    nuovi campo

--- ReserGO.Utils.DTO/Utils/DTOUserSession.cs
+++ ReserGO.Utils.DTO/Utils/DTOUserSession.cs
@@ -3,6 +3,8 @@
     public class DTOUserSession
     {
         public string Username { get; set; }
+        public string FirstName { get; set; }   
+        public string LastName { get; set; }
         public List<string> Roles { get; set; }
     }
 }
