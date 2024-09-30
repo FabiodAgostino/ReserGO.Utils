9db23d72d784f4122a8e938ed4fb4b568dd4e41c
Fabio D'Agostino <fabio.dagostino@descor.com>
2024-09-30 16:45:59 +0200

    passwordhelper

---
+++ ReserGO.Utils.DTO/Utils/PasswordHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+
+namespace ReserGO.Utils.DTO.Utils
+{
+    public class PasswordHelper
+    {
+        public static (string Hash, string Salt) HashPassword(string password)
+        {
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                var saltBytes = new byte[16];
+                rng.GetBytes(saltBytes);
+                var salt = Convert.ToBase64String(saltBytes);
+
+                using (var pbkdf2 = new Rfc2898DeriveBytes(password, saltBytes, 10000))
+                {
+                    var hashBytes = pbkdf2.GetBytes(20);
+                    var hash = Convert.ToBase64String(hashBytes);
+                    return (hash, salt);
+                }
+            }
+        }
+
+        public static bool VerifyPassword(string password, string storedHash, string storedSalt)
+        {
+            var saltBytes = Convert.FromBase64String(storedSalt);
+            using (var pbkdf2 = new Rfc2898DeriveBytes(password, saltBytes, 10000))
+            {
+                var hashBytes = pbkdf2.GetBytes(20);
+                var hash = Convert.ToBase64String(hashBytes);
+                return hash == storedHash;
+            }
+        }
+    }
+}
