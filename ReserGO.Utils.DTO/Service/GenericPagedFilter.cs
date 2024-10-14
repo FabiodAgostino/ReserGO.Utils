92fbc93386df833246fcf33dac7df7cafdff179c
Fabio D'Agostino <fabio.dagostino@descor.com>
2024-10-14 09:42:37 +0200

    filter

---
+++ ReserGO.Utils.DTO/Service/GenericPagedFilter.cs
@@ -0,0 +1,9 @@
+﻿namespace ReserGO.Utils.DTO.Service
+{
+    public class GenericPagedFilter<T>
+    {
+        public T Filter { get; set; }
+        public int? Page { get; set; }
+        public int? PageSize { get; set; }
+    }
+}
